5439f9cc80ce15a2e131008ff8cc611533e3e760
Md Munimul Islam <mishyket127@gmail.com>
2025-12-14 14:10:14 +0000

    Added auth for mqtt

--- src/publisher_mqtt.py
+++ src/publisher_mqtt.py
@@ -2,7 +2,7 @@ import json
 import time
 import paho.mqtt.client as mqtt
 
-from sensor import sense_data
+from sensor import sense_env_data
 
 MASTER_IP = '192.168.0.17'
 PORT = 1883
@@ -11,26 +11,25 @@ PUBLISH_INTERVAL = 5
 QOS = 1
 
 def main():
-    print('qsqsqs')
     client = mqtt.Client(protocol=mqtt.MQTTv5, client_id ='room1')
+    client.username_pw_set(username = 'room1', password='room1')
     client.connect(MASTER_IP, PORT, 60)
     client.loop_start()
     
     try:
         while True:
-            data = sense_data()
-            client.publish(TOPIC, json.dumps(data), qos=QOS)
+            data = sense_env_data()
             print(f"published: {data}")
+
+            client.publish(TOPIC, json.dumps(data), qos=QOS)
             time.sleep(PUBLISH_INTERVAL)
             
     except KeyboardInterrupt:
         print('Stopped data streaming')
             
     finally:
-        print('final')
         client.loop_stop()
         client.disconnect()
             
 if __name__ == '__main__':
-    print('sdd')
     main()
--- src/sensor.py
+++ src/sensor.py
@@ -4,36 +4,11 @@ import os
 import csv
 
 sense = SenseHat()
-FILE_PATH = '../data/env.csv'    
-
-def log_data(interval=5):
-    try:
-        if not os.path.exists(FILE_PATH):
-            write_csv_row(FILE_PATH, ["timestamp", "temparature", "humidity", "pressure"])
         
-        while True:
-            sensor_data = sense_data()
-            write_csv_row(FILE_PATH, sensor_data) 
-            time.sleep(interval)
-
-    except KeyboardInterrupt:
-        print('Data Logging Stopped')
-        
-def write_csv_row(path, data):
-    try:
-        with open(path, mode='a', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerow(data)
-    except:
-            print('Data coulod not be logged')
-
-        
-def sense_data():
+def sense_env_data():
     timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
     temparature = sense.get_temperature()
     humidity = sense.get_humidity()
     pressure = sense.get_pressure()
 
     return [timestamp, f"{temparature:.3f}", f"{humidity:.1f}", f"{pressure:.3f}"]
-            
-log_data()
